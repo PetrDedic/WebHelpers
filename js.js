8b6974c89683b628d1eee018db108c90d3b4eaa0
Petr Dědic <petr.dedic@pslib.cz>
2021-03-24 19:17:12 +0100

    Side Menu

--- js.js
+++ js.js
@@ -15,3 +15,42 @@ function Up() {
     behavior: "smooth",
   });
 }
+
+//menu
+$(document).ready(function () {
+  //active class toggle
+  $(function () {
+    $(".menu").on("click", function () {
+      if ($(".menu").hasClass("active")) {
+        $("*").removeClass("shown");
+        $("*").removeClass("active");
+        $("*").removeClass("active2");
+      } else {
+        $(".menu").addClass("active");
+        $(".menu-item1").addClass("shown");
+        $(".menu-item2").addClass("shown");
+        $(".menu-item3").addClass("shown");
+      }
+    });
+
+    $(".menu2").on("click", function () {
+      $(this).toggleClass("active2");
+
+      $(".menu-item31").toggleClass("shown");
+      $(".menu-item32").toggleClass("shown");
+    });
+  });
+
+  //ignore list and removing class
+  /*$(function () {
+    var ignore = Array("#menu");
+    ignore.forEach(function (item) {
+      $(item).click(function () {
+        return false;
+      });
+    });
+    $(document).on("click", function () {
+      $("#menu").removeClass("active");
+    });
+  });*/
+});
